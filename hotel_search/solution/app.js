f0321b66c33080c351aa3b3584a4b1458e067999
JerryKu <jerryku1220@gmail.com>
2018-08-06 00:49:42 -0700

    Sped up solution by making concurrent api calls

--- hotel_search/solution/app.js
+++ hotel_search/solution/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const rp = require('request-promise');
+const axios = require('axios');
 
 //scrape(): asynchronous function that sends GET requests
 //to the scraper API Endpoint for every Hotel
@@ -8,14 +8,18 @@ const rp = require('request-promise');
 //returns a merged JSON object sorted by ecstasy
 async function scrape(req, res){
   let results = [];
-  let Expedia = await rp('http://localhost:9000/scrapers/Expedia').then((res)=>{return JSON.parse(res)}).catch((err)=>{console.log(err)});
-  let Orbitz = await rp('http://localhost:9000/scrapers/Orbitz').then((res)=>{return JSON.parse(res)}).catch((err)=>{console.log(err)});
-  let Travelocity = await rp('http://localhost:9000/scrapers/Travelocity').then((res)=>{return JSON.parse(res)}).catch((err)=>{console.log(err)});
-  let Hilton = await rp('http://localhost:9000/scrapers/Hilton').then((res)=>{return JSON.parse(res)}).catch((err)=>{console.log(err)});
-  let Priceline = await rp('http://localhost:9000/scrapers/Priceline').then((res)=>{return JSON.parse(res)}).catch((err)=>{console.log(err)});
-
-  sortHotels(Expedia['results'], Orbitz['results'], Travelocity['results'], Hilton['results'], Priceline['results'], results)
-  res.send({"results":results});
+  //axios
+  axios.all([
+    axios.get('http://localhost:9000/scrapers/Expedia'),
+    axios.get('http://localhost:9000/scrapers/Orbitz'),
+    axios.get('http://localhost:9000/scrapers/Travelocity'),
+    axios.get('http://localhost:9000/scrapers/Hilton'),
+    axios.get('http://localhost:9000/scrapers/Priceline'),
+  ]).then(axios.spread((resp1,resp2,resp3,resp4,resp5)=>{
+    res.send(sortHotels(resp1.data.results, resp2.data.results, resp3.data.results,resp4.data.results,resp5.data.results,results))
+  })).catch(error => {
+    console.log(error);
+  });
 }
 
 //sortHotels(): Sorts all of the Hotels by merge sort and returns array of sorted results by ecstasy
@@ -23,14 +27,13 @@ const sortHotels = function(H1, H2, H3, H4, H5, res){
   //start all array indexes at 0
   let H1indx = H2indx = H3indx = H4indx = H5indx = 0;
   //while each index has not reached the end
-  while(H1indx !== H1.length && H2indx !== H2.length && H3indx !== H3.length && H4indx !== H4.length
-  && H5indx !== H5.length){
+  while(H1indx !== H1.length || H2indx !== H2.length || H3indx !== H3.length || H4indx !== H4.length || H5indx !== H5.length){
     //if index for a hotel has reached the end, the current ecstacy value will be 0 for that hotel;
-    let H1curr = H1[H1indx].ecstasy || 0;
-    let H2curr = H2[H2indx].ecstasy || 0;
-    let H3curr = H3[H3indx].ecstasy || 0;
-    let H4curr = H4[H4indx].ecstasy || 0;
-    let H5curr = H5[H5indx].ecstasy || 0;
+    let H1curr = H1[H1indx] ? H1[H1indx].ecstasy : -1;
+    let H2curr = H2[H2indx] ? H2[H2indx].ecstasy : -1;
+    let H3curr = H3[H3indx] ? H3[H3indx].ecstasy : -1;
+    let H4curr = H4[H4indx] ? H4[H4indx].ecstasy : -1;
+    let H5curr = H5[H5indx] ? H5[H5indx].ecstasy : -1;
     //find max of all 5 current ecstasy values
     let max = Math.max(H1curr,H2curr,H3curr,H4curr,H5curr);
     //if the max is that ecstasy value, push on the result to the result array and increase the index for that hotel
@@ -57,7 +60,7 @@ const sortHotels = function(H1, H2, H3, H4, H5, res){
         break;
     }
   }
-  return res;
+  return {"results":res};
 }
 
 //API endpoint that calls the scrape function
